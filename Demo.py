75ec923d39a72f2443c23759ef881d17a6ab6298
Laura Stracinskiene <laura.stracinskiene@gmail.com>
2025-06-20 20:08:30 +0300

    demo.py

--- Demo.py
+++ Demo.py
@@ -1,74 +1,21 @@
-import os
-from langchain import hub
-
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 import streamlit as st
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.vectorstores import InMemoryVectorStore
+
+import os
+from langchain_openai.chat_models import ChatOpenAI
 
 from dotenv import load_dotenv
-from langchain_openai.chat_models import ChatOpenAI
-from langchain_community.document_loaders import WebBaseLoader
-import bs4  # BeautifulSoup for parsing HTML
 
 load_dotenv()
 
-token = os.getenv("SECRET")  # Replace with your actual token
-endpoint = "https://models.github.ai/inference"
-model = "openai/gpt-4.1-nano"
+token = os.getenv("SECRET")  
+model = "gpt-4.1-nano"
 
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2017-06-21-overview/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
-)
-docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
-splits = text_splitter.split_documents(docs)
-
-embeddings=OpenAIEmbeddings(
-    model="text-embedding-3-small",
-    base_url="https://models.inference.ai.azure.com",
-    api_key=token, # type: ignore
-)
-
-vectorstore = InMemoryVectorStore(embeddings)
-
-_ = vectorstore.add_documents(documents=splits)
-
-retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
-
-def format_docs(docs):
-    print(docs)
-    return "\n\n".join(doc.page_content for doc in docs)
 
 st.title("Streamlit LangChain Demo")
 
 def generate_response(input_text):
-    llm = ChatOpenAI(base_url=endpoint, temperature=0.7, api_key=token, model=model)
+    llm = ChatOpenAI(temperature=0.7, api_key=token, model=model)
+    st.info(llm.invoke(input_text))
 
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
-        )
-    
-    
-    st.info(rag_chain.invoke(input_text))
-
-with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
-    )
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        generate_response(text)
+generate_response('hello')
